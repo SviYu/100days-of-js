857f207c7ae172397ca49f221fb2d931deaab531
Svitlana Yukhymenko <yuhimenko.svieta@gmail.com>
2023-02-21 20:43:21 +0100

    added js code to day 24

--- day24/script.js
+++ day24/script.js
@@ -1,43 +1,54 @@
-const msg = new SpeechSynthesisUtterance();
-let voices = [];
+const nav = document.getElementById('main');
+const topOfNav = nav.offsetTop;
+const logo = document.querySelector('.logo')
 
-const voicesDropdown = document.querySelector('[name="voice"]');
-const options = document.querySelectorAll('[type="range"], [name="text"]');
-const speakBtn = document.getElementById('speak');
-const stopBtn = document.getElementById('stop');
-msg.text = document.querySelector('[name="text"]').value;
-
-function populateVoices(){
-    voices = this.getVoices();
-    //console.log(voices);
-    const voiceOptions = voices
-        .filter(voice => voice.lang.includes('en'))
-        .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
-        .join('');
-    voicesDropdown.innerHTML = voiceOptions;
-}
-
-function setVoice(){
-    console.log(this.value);
-    msg.voice = voices.find(voice => voice.name === this.value);
-    //toggle();
-}
-
-function toggle(startOver = true){
-    speechSynthesis.cancel();
-    if(startOver){
-        speechSynthesis.speak(msg);
+function fixNav(){
+    console.log(topOfNav, window.scrollY);
+   if(window.scrollY >= topOfNav){
+        document.body.classList.add('fixed-nav');
+    }else{
+       document.body.classList.remove('fixed-nav');
     }
 }
 
-function setOption(){
-    console.log(this.name, this.value);
-    msg[this.name] = this.value;
-    //toggle();
-}
+//Slide-in function from Day 13
 
-speechSynthesis.addEventListener('voiceschanged', populateVoices);
-voicesDropdown.addEventListener('change', setVoice);
-options.forEach(option => option.addEventListener('change', setOption));
-speakBtn.addEventListener('click', toggle);
-stopBtn.addEventListener('click', () => toggle(false));
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function () {
+    var context = this, args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+const slideImages = document.querySelectorAll('.slide-in');
+
+function checkSlide() {
+  
+  slideImages.forEach(slideImage => {
+    //half way through the image
+    const slideInAt = (window.scrollY + window.innerHeight) - slideImage.height / 2;
+    //bottom of the image
+    const imageBottom = slideImage.offsetTop + slideImage.height;
+    const halfShown = slideInAt > slideImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (halfShown && isNotScrolledPast) {
+      slideImage.classList.add('active')
+    } else {
+        slideImage.classList.remove('active')
+    }
+    console.log('bla')
+  })
+  
+}
+window.addEventListener('scroll', fixNav)
+window.addEventListener('scroll', debounce(checkSlide))
+
+
